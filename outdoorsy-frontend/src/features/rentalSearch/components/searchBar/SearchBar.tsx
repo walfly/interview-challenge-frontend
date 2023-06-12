70ba8df2d1d53ca85d8da43f13646b262efe1cfb
Walker Flynn <walker@oliver.space>
2023-06-12 08:58:55 -0700

    fix spacing

--- outdoorsy-frontend/src/features/rentalSearch/components/searchBar/SearchBar.tsx
+++ outdoorsy-frontend/src/features/rentalSearch/components/searchBar/SearchBar.tsx
@@ -2,20 +2,20 @@ import { ChangeEvent, FC } from "react";
 import styles from "./SearchBarStyle.module.css"
 
 type SearchBarProps = {
-    value: string,
-    onChange: (event: ChangeEvent<HTMLInputElement>) => void,
+  value: string,
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void,
 };
 
 const SearchBar: FC<SearchBarProps> = ({ value, onChange }) => {
-    return (<>
-        <input
-            className={styles.input}
-            defaultValue={value}
-            onChange={(e) => onChange(e)}
-            type="text"
-            placeholder="Search"
-        />
-    </>);
+  return (<>
+    <input
+      className={styles.input}
+      defaultValue={value}
+      onChange={(e) => onChange(e)}
+      type="text"
+      placeholder="Search"
+    />
+  </>);
 }
 
 export default SearchBar;
